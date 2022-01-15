b77700f75cd21811c148e997af6013f5bb76465f
liuyunan <liuyunan.20@gmail.com>
2022-01-15 00:37:30 -0800

    computer have a random option, print the result

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -1,4 +1,13 @@
 # Write your code here
+import random
+
+win_pairs = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+
+computer_option = random.choice(["rock", "paper", "scissors"])
 user_option = input()
-computer_options = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
-print(f"Sorry, but the computer chose {computer_options[user_option]}")
+if user_option == computer_option:
+    print(f"There is a draw ({user_option})")
+elif user_option == win_pairs[computer_option]:
+    print(f"Well done. The computer chose {computer_option} and failed")
+elif computer_option == win_pairs[user_option]:
+    print(f"Sorry, but the computer chose {computer_option}")
