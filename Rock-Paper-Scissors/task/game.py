c5306db16698659fbec8ec1712f6b221d7bdda94
liuyunan <liuyunan.20@gmail.com>
2022-01-15 00:02:45 -0800

    computer always win

---
+++ Rock-Paper-Scissors/task/game.py
@@ -0,0 +1,4 @@
+# Write your code here
+user_option = input()
+computer_options = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+print(f"Sorry, but the computer chose {computer_options[user_option]}")
